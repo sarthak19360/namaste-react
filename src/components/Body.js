7127476173123e89c4f4fd8e1af69a874810008d
sarthak19360 <sarthak19360@gmail.com>
2023-12-24 23:17:00 +0530

    Removed mockData.js and used swiggy api to fetch live data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,39 @@
 import ResCard from "./ResCard";
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/api/seo/getListing?lat=28.67003492726483&lng=77.11469986756225"
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json.data.success.cards[1].card.card.gridElements.infoWithStyle
+        .restaurants
+    );
+  };
+
   return (
     <div className="body">
       <div className="search-bar">
-        <input type="text" placeholder="Search for Restaurants" />
-        <button>Search</button>
+        <button
+          onClick={() => {
+            const filteredRestaurants = listOfRestaurants.filter((res) => {
+              return res.info.avgRating > 4.3;
+            });
+            setListOfRestaurants(filteredRestaurants);
+          }}
+        >
+          Search for Top Rated Restaurants
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <ResCard key={restaurant.info.id} restaurant={restaurant} />
         ))}
       </div>
